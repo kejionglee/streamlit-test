ee7c539b5789d88ba9d16a36715726168ae00e99
KEJIONG_LEE <kejiong.lee@dell.com>
2025-07-03 10:50:49 +0800

    .

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.image("images.jpg")
+st.title("Ogeiiii")
+
+st.date_input("Transcation date")
+st.radio("Department:",['AI','CFI PE','ISG NPI','CSG NPI'])
+
+st.camera_input("Case reported")
